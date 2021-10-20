8c8b1072f575fd119c9127146b6ac9ace56e07d3
[edwinp95] <[ewhalen@uncc.edu]>
2021-10-20 19:03:22 -0400

    Added Display Results Functionality

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,11 +1,21 @@
 const person = ["Will Bettleheim", "Joe Goldberg", "Beck Something", "Love Quinn"];
-const salaries = [];
+const salaries = [50000, 80000, 120000];
 
 function addSalary(){
     alert("Salary: ");
 }
 
 function displayResults(){
+    let average = 0;
+    let sum = 0;
+    let maxSal = Math.max(...salaries);
+    for(var i = 0; i < salaries.length; i++){
+        sum += salaries[i];
+    }
+    average = sum / salaries.length;
+    average = Math.round(average);
+    document.getElementById("average").innerHTML = "$" + average;
+    document.getElementById("highest").innerHTML = "$" + maxSal;
 
 }
 
