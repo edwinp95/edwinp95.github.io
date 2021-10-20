82eb0babe4c698a1f4044b44370b66ac91fbc953
[edwinp95] <[ewhalen@uncc.edu]>
2021-10-20 19:44:40 -0400

    Display Salary Complete

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,5 +1,6 @@
-const person = ["Will Bettleheim", "Joe Goldberg", "Beck Something", "Love Quinn"];
-const salaries = [50000, 80000, 120000];
+const person = ["Will Bettleheim", "Joe Goldberg", "Beck S.", "Love Quinn"];
+const salaries = [50000, 80000, 120000, 43000];
+const merged = [];
 
 function addSalary(){
     alert("Salary: ");
@@ -20,5 +21,15 @@ function displayResults(){
 }
 
 function displaySalary(){
-
+    var table = document.getElementById("results_table");
+        for(var i =0; i< person.length; i++){
+        merged.push([person[i],salaries[i]]);
+   }
+    for(var i=1; i<table.rows.length; i++){
+        for(var j = 0; j< table.rows[i].cells.length; j++){
+            table.rows[i].cells[j].innerHTML = merged[i-1][j];
+            
+       }
+    }
+    console.log(merged);
 }
