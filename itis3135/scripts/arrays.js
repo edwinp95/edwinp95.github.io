0dbee1331f5a6769fc0c651e9ec72a882b23e806
[edwinp95] <[ewhalen@uncc.edu]>
2021-10-20 18:49:41 -0400

    Arrays Basics

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,14 @@
+const person = ["Will Bettleheim", "Joe Goldberg", "Beck Something", "Love Quinn"];
+const salaries = [];
+
+function addSalary(){
+    alert("Salary: ");
+}
+
+function displayResults(){
+
+}
+
+function displaySalary(){
+
+}
