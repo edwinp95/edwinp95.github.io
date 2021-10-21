5d1b62160175c51a9c0bbdf4d32964c0b528e833
[edwinp95] <[ewhalen@uncc.edu]>
2021-10-20 20:13:31 -0400

    Updates

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -2,8 +2,16 @@ const person = ["Will Bettleheim", "Joe Goldberg", "Beck S.", "Love Quinn"];
 const salaries = [50000, 80000, 120000, 43000];
 const merged = [];
 
+for(var i =0; i< person.length; i++){
+    merged.push([person[i],salaries[i]]);
+}
+
 function addSalary(){
-    alert("Salary: ");
+    newName = prompt("Employee Name: ", "John Doe");
+    newSalary = parseInt(prompt("Salary: ", "50000"));
+    merged.push([newName, newSalary]);
+    person.push(newName);
+    salaries.push(newSalary);
 }
 
 function displayResults(){
@@ -22,14 +30,12 @@ function displayResults(){
 
 function displaySalary(){
     var table = document.getElementById("results_table");
-        for(var i =0; i< person.length; i++){
-        merged.push([person[i],salaries[i]]);
-   }
+
     for(var i=1; i<table.rows.length; i++){
         for(var j = 0; j< table.rows[i].cells.length; j++){
             table.rows[i].cells[j].innerHTML = merged[i-1][j];
-            
        }
     }
+
     console.log(merged);
 }
